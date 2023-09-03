fb4a96febcfd9a95cd53d09edb1657b57140135f
Akash Dholakiya <akashdholakiya222@gmail.com>
2023-09-03 23:57:26 +0530

    "Next Update"

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const  uri = "mongodb://127.0.0.1:27017/test";
+
 const MongoConnect = () => {
     mongoose.connect(uri);
     console.log("Connection Successful");
-}
+}  
 
 module.exports = MongoConnect;
